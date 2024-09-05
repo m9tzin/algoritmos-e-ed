883a2460baacbe6fff2d950345fc795724457795
m9tzin <matheusmrno@gmail.com>
2024-09-05 08:31:20 -0300

    new

---
+++ Aula01/e10-maiormenorbonus.c
@@ -0,0 +1,12 @@
+/* Estrutura de Dados I
+Matheus Marinho */
+
+/* BONUS TRACK! Elabore um programa que faça leitura de vários números inteiros,
+até que se digite um número negativo. O programa tem que retornar o maior e o
+menor número lido. */
+
+#include <stdio.h>
+
+int main(){
+    return 0;
+}
