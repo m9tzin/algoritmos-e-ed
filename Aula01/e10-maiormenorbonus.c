cdc1cbd9eb6e8d3d4170843d6ad82e5700894ff2
m9tzin <matheusmrno@gmail.com>
2024-09-05 18:11:26 -0300

    changing exercises

--- Aula01/e10-maiormenorbonus.c
+++ Aula01/e10-maiormenorbonus.c
@@ -8,5 +8,28 @@ menor número lido. */
 #include <stdio.h>
 
 int main(){
-    return 0;
+    int num, menor, maior;
+
+    printf("Insira um numero inteiro: (Numero negativo = encerra o programa.)\n");
+    scanf("%d", &num);
+    
+    maior = num;
+    menor = num;
+
+    while(num > 0){
+        if (num > maior){
+            maior = num;
+        }
+        if (num < menor){
+            menor = num;
+        }
+        printf("Insira um numero inteiro: (Numero negativo = encerra o programa.)\n");
+        scanf("%d", &num);
+    }
+        
+        printf("O maior numero foi [%d] e o menor foi [%d].\n", maior, menor);
+
+        printf("Programa encerrado.");
+
+     return 0;
 }
