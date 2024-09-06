d6ac0ca309799181586491ad8f1c9108044bacd0
m9tzin <matheusmrno@gmail.com>
2024-09-06 19:14:51 -0300

    enum

--- Aula02/enum.c
+++ Aula02/enum.c
@@ -5,12 +5,64 @@ Matheus Sousa Marinho */
 
 #include <stdio.h>
 
-enum Status { LOW, MEDIUM, HIGH };
+enum Status { LOW = 5, MEDIUM = 50, HIGH = 100 };
 enum Semana { Seg = 1, Ter = 2, Qua = 3, Qui = 4, Sex = 5, Sab = 6, Dom = 7};
 
 int main() {
-    enum Status resultado = MEDIUM;
-    enum Semana dia = Ter;
+    enum Status resultado = HIGH;
+    enum Semana dia = Sex;
     
+    /* Utilizando o switch case... */
+
+    switch (dia) {
+        case 1:
+        printf("monday.\n");
+        break;
+
+        case 2:
+        printf("tuesday.\n");
+        break;
+
+        case 3:
+        printf("wednesday.\n");
+        break;
+
+        case 4:
+        printf("thursday.\n");
+        break;
+
+        case 5:
+        printf("friday.\n");
+        break;
+
+        case 6:
+        printf("saturday.\n");
+        break;
+
+        case 7:
+        printf("sunday.\n");
+        break;
+
+        default:
+        printf("looking forward to the weekend, try again :)");
+    }
+
+    switch (resultado){
+        case LOW:
+        printf("help! energy status: [%d]", resultado);
+        break;
+
+        case MEDIUM:
+        printf("Be careful, charge your phone! energy status: [%d]", resultado);
+        break;
+
+        case HIGH:
+        printf(":) energy status: [%d]", resultado);
+        break;
+
+        default:
+        printf("Default message to your battery.");    
+    }
+
     return 0;
 }
