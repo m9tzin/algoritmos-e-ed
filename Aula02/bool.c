56288bb3dcf0d90b3cc25057c64cf221e85e55fc
m9tzin <matheusmrno@gmail.com>
2024-09-08 15:56:27 -0300

    .

--- Aula02/bool.c
+++ Aula02/bool.c
@@ -2,6 +2,8 @@
 Matheus Sousa Marinho */
 
 /* Estude bool.c  e explique como funciona o tipo de dado booleano e quais seriam possíveis utilidades num código. */
+/* O dado boolean sempre retorna valores verdadeiros ou falsos, uma possivel utilidade seria em condicionais
+fazendo com que realize determinada tarefa caso algo seja verdadeiro e outra caso seja falso. */
 
 #include <stdio.h>
 #include <stdbool.h>
