3736d8e8007a1c8925b5556b5dc195604c1dcd3e
m9tzin <matheusmrno@gmail.com>
2024-09-08 14:37:06 -0300

    comment

--- Aula01/Lista01/e01-cometa.c
+++ Aula01/Lista01/e01-cometa.c
@@ -1,6 +1,12 @@
 /* Estrutura de Dados I
 Matheus Marinho */
 
+/* Escreva um programa C que, dado o ano atual, determine qual
+é o próximo ano em que o cometa Halley será visível novamente.
+Se o ano atual é um ano de passagem do cometa, considere que o
+cometa já passou nesse ano, ou seja, considere sempre o próximo
+ano de passagem após o atual. */
+
 #include <stdio.h>
 
 int main(){
