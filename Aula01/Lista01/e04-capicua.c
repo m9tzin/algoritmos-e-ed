7805b9d7004b396899839db1034c4407f0ca8884
m9tzin <matheusmrno@gmail.com>
2024-09-09 21:25:32 -0300

    capicua init

--- Aula01/Lista01/e04-capicua.c
+++ Aula01/Lista01/e04-capicua.c
@@ -1,6 +1,16 @@
 /* Estrutura de Dados I
 Matheus Marinho */
 
+/* Alan deve pegar um número com quatro dígitos e verificar se o reverso desse número é ele mesmo. Se for, Alan deve responder "sim", caso contrário, deve responder "não".
+
+Escreva um programa em C que implemente essa verificação conforme descrita a seguir.
+
+Entrada
+Um inteiro n ≥ 1 representando a quantidade de números que Alan deve analisar.
+n números inteiros de até quatro dígitos.
+Saída
+As frases "sim" ou "não", que correspondem à resposta para cada número analisado. */
+
 #include <stdio.h>
 
 int main(){
