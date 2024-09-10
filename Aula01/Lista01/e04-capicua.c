16cd5931fcf08c73ea2da3090123c47a6f7e5ced
m9tzin <matheusmrno@gmail.com>
2024-09-10 08:23:29 -0300

    capicua

--- Aula01/Lista01/e04-capicua.c
+++ Aula01/Lista01/e04-capicua.c
@@ -13,6 +13,40 @@ As frases "sim" ou "não", que correspondem à resposta para cada número analis
 
 #include <stdio.h>
 
+void palindromoCheck(int number){
+    int original = number, inverse = 0, lastDigit;
+
+    /* Inverter o numero */
+    while(number != 0){
+        lastDigit = number % 10; /* Ultimo digito */
+        inverse = inverse * 10 + lastDigit; /* Construindo o inverso */
+        number /= 10; /* Remover o ultimo digito da esquerda */
+    }
+
+    if(inverse == original){
+        printf(" yes");
+    }else{
+        printf(" no");
+    }
+}
+
+void verify(int limit){
+    int array[limit];
+    for(int i=0; i<limit; i++){
+        printf("Insira um numero: \n");
+        scanf("%d", &array[i]);
+    }
+    for(int k=0; k<limit; k++){
+        palindromoCheck(array[k]); 
+    }
+}
+
 int main(){
+    int limit;
+
+    printf("Quantos numeros serao analisados? \n");
+    scanf("%d", &limit);
+    verify(limit);
+
     return 0;
 }
---
+++ Aula01/Lista01/e05-computacao.c
@@ -0,0 +1,9 @@
+/* Estrutura de Dados I
+Matheus Marinho */
+
+/*  */
+
+#include <stdio.h>
+int main (void){
+    return 0;
+}
