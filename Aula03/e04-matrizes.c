c1c84f2c416fadfe5be26ffe76a87e83d5557550
m9tzin <matheusmrno@gmail.com>
2024-09-13 18:51:08 -0300

    matrix

---
+++ Aula03/e04-matrizes.c
@@ -0,0 +1,17 @@
+/* Estrutura de Dados I
+Matheus Marinho */
+
+/* Dada uma quantidade de tempo N, crie uma única variável para armazenar em simultâneo dados de pressao, temperatura e humidade.
+
+float sensor[DIM1][DIM2];
+// DIM1: Indice do tipo de sensor
+// DIM2: Tamanho dos dados */
+
+#include <stdio.h>
+
+int main(void){
+    int i,j;
+    float sensor[i][j]; /* Matriz de dados do sensor */
+
+    return 0;
+}
