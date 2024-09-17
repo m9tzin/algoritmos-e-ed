5a5b49c55cd8a07cb32c688da0c24d0dab5239c1
m9tzin <matheusmrno@gmail.com>
2024-09-17 20:14:17 -0300

    asd

--- Aula01/Lista01/e06-envelopes.c
+++ Aula01/Lista01/e06-envelopes.c
@@ -1,17 +1,6 @@
 /* Estrutura de Dados I
 Matheus Marinho */
 
-/*  */
-
-#include <stdio.h>
-
-int main() {
-    return 0;
-}
-
-/* Estrutura de Dados I
-Matheus Marinho */
-
 /* Por exemplo, se a BM disser que produz três tipos de balas (A, B, C) e uma pessoa tem três rótulos de A, três rótulos de B e dois rótulos de C, ela pode enviar no máximo dois envelopes, já que falta um rótulo de C para compor o terceiro envelope. Sabe-se que não há limite para o número de envelopes que uma pessoa pode enviar. Kurt gosta muito das balas e, por isso, a quantidade de rótulos que ele tem é muito grande. No entanto, ele não está conseguindo determinar a quantidade máxima de envelopes que pode enviar. Como você conhece o Kurt, ele pediu sua ajuda para fazer o cálculo, de modo que ele compre o número exato de envelopes necessários para enviar para a promoção.
 
 Escreva um programa em C que, a partir da lista de rótulos de Kurt, calcule o número máximo de envelopes válidos que ele pode enviar para a promoção. */
@@ -19,7 +8,7 @@ Escreva um programa em C que, a partir da lista de rótulos de Kurt, calcule o n
 #include <stdio.h>
 
 void count(){
-    return 1;
+    return 0;
 }
 
 void distribuir(){
