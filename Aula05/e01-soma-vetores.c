e641dfbdb47757bf19f518f85d758b0b27df4160
m9tzin <matheusmrno@gmail.com>
2024-09-27 11:59:17 -0300

    e01

--- Aula05/e01-soma-vetores.c
+++ Aula05/e01-soma-vetores.c
@@ -2,7 +2,56 @@
 Matheus Marinho */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char* argv[]){
+# define VMAX 100
+# define VMIN 1
+
+void somaVetor(int *p, int *q, int tamanho) {
+    for(int i=0; i<tamanho; i++){
+        p[i] += q[i];
+    }
+}
+
+void mostraVetor(int *p, int tamanho) {
+    for(int i=0; i<tamanho; i++){
+        printf(" [%d] ", p[i]);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    srand(time(NULL));
+    int N; // tamanho do vetor \
+              (inferir a partir dos dados de entrada)
+
+    N = argc-1; // except the program name
+    int v1[N]; // dado
+    int v2[N]; // aleatorio
+
+    // Vetor dado
+    for(int i=0; i<N; i++){
+        v1[i] = atoi(argv[i+1]);
+    }
+
+    // Vetor aleatorio
+    for(int i=0; i<N; i++){
+        v2[i] = rand() % ((VMAX-VMIN+1)+VMIN);
+    }
+
+    printf("Vetor 1: ");
+    mostraVetor(v1, N);
+    printf("\n");
+    
+    printf("Vetor 2: ");
+    mostraVetor(v2, N);
+    printf("\n");
+    
+    somaVetor(v1, v2, N);
+    
+    printf("Soma   : ");
+    mostraVetor(v1, N);
+    printf("\n");
+
     return 0;
 }
