d4b259909620e7b15bf33fd4084fd942ab318496
m9tzin <matheusmrno@gmail.com>
2024-09-29 17:05:41 -0300

    e05

--- Aula04/e05-ponteiros.c
+++ Aula04/e05-ponteiros.c
@@ -9,11 +9,26 @@ A função deverá armazenar o dobro de A na própria variável A e o dobro de B
 #include <stdio.h>
 #include <stdlib.h>
 
-void somaDobro(int numero1, int numero2){
-    int resultado;
-    return resultado;
+int somaDobro(int *numero1, int *numero2){
+    *numero1 = *numero1 * 2;
+    *numero2 = *numero2 * 2;
+    return *numero1 + *numero2;
 }
 
 int main(void){
+    int a, b;
+    int *ptr_a, *ptr_b;
+
+    printf("Insira um valor para A:\n");
+    scanf("%d", &a);
+    printf("Insira um valor para B:\n");
+    scanf("%d", &b);
+
+    ptr_a = &a;
+    ptr_b = &b;
+
+    somaDobro(ptr_a, ptr_b);
+    printf("A soma do dobro dos numeros lidos e: %d", somaDobro(ptr_a, ptr_b));
+    
     return 0;
 }
