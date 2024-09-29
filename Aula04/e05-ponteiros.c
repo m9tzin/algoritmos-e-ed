45b6e3b384cd86cbf079a19026d18de3de038e54
m9tzin <matheusmrno@gmail.com>
2024-09-29 16:33:09 -0300

    e05

---
+++ Aula04/e05-ponteiros.c
@@ -0,0 +1,19 @@
+/* Estrutura de Dados I
+Matheus Marinho */
+
+/* Elaborar um programa que leia dois valores inteiros (A e B).
+Em seguida faça uma função que retorne a soma do dobro dos dois números lidos.
+A função deverá armazenar o dobro de A na própria variável A e o dobro de B na própria variável B
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void somaDobro(int numero1, int numero2){
+    int resultado;
+    return resultado;
+}
+
+int main(void){
+    return 0;
+}
