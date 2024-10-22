65b56401021a22c2184ec595e5d3613631946b08
m9tzin <matheusmrno@gmail.com>
2024-10-22 08:28:05 -0300

    .

--- Aula06/e01-vetores-dinamicos.c
+++ Aula06/e01-vetores-dinamicos.c
@@ -1,9 +1,13 @@
 /* Estrutura de Dados I
 Matheus Marinho */
 
+/* Escreva um programa para armazenar um nome de até N bytes digitados pelo usuário: */
+/* Requisitos:
+Verifique que a memória seja alocada corretamente e caso não seja informe ao usuário
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
+int main(int argc, char* argv[]){
     return 0;
 }
