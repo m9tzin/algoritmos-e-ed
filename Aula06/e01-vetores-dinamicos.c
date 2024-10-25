b71244200f372ed60eac6106bcec8f07d863321b
m9tzin <matheusmrno@gmail.com>
2024-10-25 10:55:19 -0300

    e01

--- Aula06/e01-vetores-dinamicos.c
+++ Aula06/e01-vetores-dinamicos.c
@@ -7,7 +7,30 @@ Verifique que a memória seja alocada corretamente e caso não seja informe ao u
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]){
+    char *p; /* pointer to char */
+    int n; /* número de bytes */
+
+     if(argc == 2){
+        n = atoi(argv[1]); 
+        p = (char*)malloc(n);
+
+        if(p == NULL){
+            printf("Memoria Insuficiente.\n");
+            return -1;
+        }
+        printf("Insira o nome: ");
+        fgets(p, n, stdin);
+
+        printf("Nome: %s\n", p);
+
+        free(p);
+
+    }else{
+        printf("Parametro nao informado.\n");  
+    } 
+    
     return 0;
 }
