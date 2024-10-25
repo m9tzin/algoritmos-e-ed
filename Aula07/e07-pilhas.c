1ecc2c68ede27df80880ea56713e9be7de776f42
m9tzin <matheusmrno@gmail.com>
2024-10-25 09:38:13 -0300

    aula07

---
+++ Aula07/e07-pilhas.c
@@ -0,0 +1,10 @@
+/* Estrutura de Dados I
+Matheus Marinho */
+
+/* Dado duas pilhas P1 e P2 que armazenam números inteiros, faça um programa que tenha as seguintes funções:  */
+
+/* Uma função para testar se as duas pilhas P1 e P2 são iguais. Caso não forem iguais diga qual pilha é maior. Uma função que forneça o maior, o menor e a média aritmética dos elementos de uma pilha. Uma função para transferir elementos de P1 para P2 (copia). Uma função para retornar o número de elementos de uma pilha que possuem valor ımpar. Uma função para retornar o número de elementos da uma pilha que possuem valor par  */
+
+int main(void){
+    return 0;
+}
