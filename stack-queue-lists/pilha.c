fd4d32668ae3617fc27300f9a4d10d1e477f5609
m9tzin <matheusmrno@gmail.com>
2024-11-11 21:10:36 -0300

    ststack

--- stack-queue-lists/pilha.c
+++ stack-queue-lists/pilha.c
@@ -8,18 +8,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-	struct Elemento{
-		int num;
-		struct Elemento *prox;
-	};
+typedef struct Elemento{
+	int num;
+	struct Elemento *prox;
+}Elemento;
 
-typedef	struct Elemento Elemento;
 
-	struct Pilha{
-		struct Elemento *topo;
-	};
+typedef struct Pilha{
+	struct Elemento *topo;
+}Pilha;
 	
-typedef struct Pilha Pilha;
 
 // Pointer auxiliar
 	Elemento *aux;
@@ -29,24 +27,34 @@ Pilha* criarPilha(){
 	if(pilha != NULL){
 		pilha->topo = NULL;
 	}
-
 	return pilha;
 }
 
 void inserirElemento(Pilha *p){
-
 	Elemento *novo = (Elemento*)malloc(sizeof(Elemento));
-	
 	printf("Insira o novo elemento: ");
 	scanf("%d", &novo->num);
+	
 	novo->prox = p->topo;
 	p->topo = novo;
 
 }
 
+void retirarElemento(Pilha *p){
+	if(p->topo == NULL){
+		printf("Empty stack.");
+	}else{
+		aux = p->topo;
+		printf("[%d] foi removido!\n", p->topo->num);
+		p->topo = p->topo->prox;
+		
+		free(aux);
+	}
+}
+
 void consultarPilha(Pilha *p){
 	if(p->topo == NULL){
-		printf("EMpty");
+		printf("Empty stack.");
 	}else{
 		Elemento *aux = p->topo; // pointer para o topo
 		do{
@@ -58,15 +66,17 @@ void consultarPilha(Pilha *p){
 }
 
 int main(void){
-	
 	Pilha *stack = criarPilha();
 
-	for(int i=0; i<10; i++){
+	for(int i=0; i<5; i++){
 	inserirElemento(stack);
 	}
-	
+	printf("\n\n");	
 	consultarPilha(stack);
-	
-	
+	printf("\n\n");
+	retirarElemento(stack);
+	printf("\n\n");
+	consultarPilha(stack);
+
 	return 0;
 }
