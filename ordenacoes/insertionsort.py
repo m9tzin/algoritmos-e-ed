d05956c285f34fa0a60d955f68cdc3b8fd42f21a
Gustavo Ferreira <Gusta>
2025-03-15 11:43:35 -0300

    Create python Insertion Sort

--- ordenacoes/insertionsort.py
+++ ordenacoes/insertionsort.py
@@ -1,3 +1,27 @@
 # Estrutura de Dados II
 # Matheus Marinho
 # Insertion Sort
+
+def insertionSort(arr):
+    n = len(arr)
+
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        aux = i
+        while j >= 0 and key < arr[j]:
+            arr[aux] = arr[j]
+            arr[j] = key
+            j = j - 1
+            aux = aux - 1
+    return arr
+
+# Input:    
+arr = [50, 16, 23, 5, 7, 12, 10]
+
+# Output:
+# Array original:  [50, 16, 23, 5, 7, 12, 10]
+# Array ordenado:  [5, 7, 10, 12, 16, 23, 50]
+
+print("Array original: ", arr)
+print("Array ordenado: ", insertionSort(arr))
