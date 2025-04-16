f2d398be6e34f753a818b03b409f2cb32b1207d0
Matheus Marinho <107821264+m9tzin@users.noreply.github.com>
2025-04-16 21:52:49 +0000

    boolean bubble

---
+++ ordenacoes/bubblesort-bool.c
@@ -0,0 +1,46 @@
+/* Estrutura de Dados II
+    Matheus Marinho 
+    Bubble Sort com boolean improvement */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void bubbleSortbool(int arr[], int size){
+    int aux;
+    bool swap;
+
+    for(int i = 0; i <= size-1; i++){
+        swap = false;
+        for(int j = 0; j<= size-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap = true;
+                aux = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = aux;
+            }
+        }
+    }
+
+}
+
+int main(){
+    int arr[] = {5, 8, 2, 10, 15, 9, 25, 27, 23, 40};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Vetor original: \n");
+    for(int i=0; i<=size-1; i++){
+        printf("[%d]", arr[i]);
+    }
+    printf("\n");
+    bubbleSortbool(arr, size);
+    printf("\n");
+    
+    printf("Vetor ordenado: \n");
+    for(int i=0; i<=size-1; i++){
+        printf("[%d]", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
