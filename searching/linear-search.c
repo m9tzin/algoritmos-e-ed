2b3110cb6c0cb19d19b0fd3fcbadfddce8cd3fad
m9tzin <matheusmrno@gmail.com>
2025-04-26 20:32:42 -0300

    linear-search.c

---
+++ searching/linear-search.c
@@ -0,0 +1,16 @@
+/* Estrutura de Dados II
+    26/04/2025
+    Matheus Marinho */
+
+    /* Linear Search */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void linearSearch(){
+    
+}
+
+int main(){
+    return 0;
+}
