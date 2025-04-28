96552d9c7ae8d049c6c8f26ae30fc196cb894508
Matheus Marinho <107821264+m9tzin@users.noreply.github.com>
2025-04-28 12:41:23 +0000

    struct linear

---
+++ searching/linear-search.c
@@ -0,0 +1,19 @@
+/* Linear Search implementation
+    Matheus Marinho
+    */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int index;
+    int value;
+}Elemento;
+
+int linearSearch(){
+    return idx;
+}
+
+int main(int argc, char* argv[]){
+    return 0;
+}
