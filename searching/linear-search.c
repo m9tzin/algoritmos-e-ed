c9dce63946fe5e1a85f75275e97487a4ad787062
Matheus Marinho <107821264+m9tzin@users.noreply.github.com>
2025-04-28 13:11:15 +0000

    linear-search

--- searching/linear-search.c
+++ searching/linear-search.c
@@ -10,11 +10,40 @@ typedef struct{
     int value;
 }Elemento;
 
-int linearSearch(int arr[], int size){
-    int idx;
-    return idx;
+int linearSearch(Elemento arr[], int size, int target){
+    if(arr == NULL || size == 0){
+        return -1;
+    }
+    for(int i = 0; i <= size-1; i++){
+        if(arr[i].value == target){
+            return arr[i].index;
+        }
+    }
+    return -1;
 }
 
 int main(int argc, char* argv[]){
+    Elemento elementos[] = {
+        {0, 10}, {1, 2}, {2, 3}, {3, 7}, {4, 23},
+        {5, 1}, {6, 9}, {7, 12}, {8, 11}, {9, 4}
+    };
+
+    if (argc != 2) {
+        printf("Incorrect value to target. Please, insert again.\n");
+        return 1;
+    }
+    /* Values */
+    int size = sizeof(elementos) / sizeof(elementos[0]);
+    int target = atoi(argv[1]);
+    int idx = linearSearch(elementos, size, target);
+    
+    /* ./linearsearch <target> */
+
+    if(idx == -1){
+        printf("Value not found.\n");
+    }
+    else{
+        printf("Value found. Index: [%d]\n", idx);
+    }
     return 0;
 }
