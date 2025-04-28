41f5195ec8024d2288cd60e5d3461eed3db68ad3
Matheus Marinho <107821264+m9tzin@users.noreply.github.com>
2025-04-28 14:18:33 +0000

    hash

---
+++ searching/hashtable.c
@@ -0,0 +1,57 @@
+/* Hash Table
+    Matheus Marinho */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define table_size 10
+
+typedef struct{
+    int key;
+    int value;
+    struct Item* next; // pointer to next item
+}Item;
+
+Item* hashtable[table_size];
+
+int hashfunc(){
+    
+}
+
+Item* createItem(int key, int value){
+    Item* novoItem = malloc(sizeof(Item));
+    novoItem->key = key;
+    novoItem->value = value;
+    novoItem->next = NULL;
+    return novoItem;
+}
+
+void print(Item* p){
+    for(int i = 0; i <= table_size-1; i++){
+        Item* current = p;
+        while(current != NULL){
+            printf("key: {%d}, value: [%d]", current->key, current->value);
+            current = current->next;
+        }
+    }
+}
+
+void insert(){
+
+}
+
+int search(){
+
+}
+
+void generateRandomData() {
+    srand(time(NULL));   
+    for (int i = 0; i < 10; i++) {
+        int key = rand() % 100;  
+        int value = rand() % 100;  
+        insert(key, value);
+    }
+}
+
+int main(int argc, char* argv[]){
+    return 0;
+}
