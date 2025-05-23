6af0b166a54053999028cc83c44483db81a1e630
Matheus Marinho <107821264+m9tzin@users.noreply.github.com>
2025-05-23 14:45:10 +0000

    inserir

--- árvores/avl-tree/avl.c
+++ árvores/avl-tree/avl.c
@@ -22,31 +22,35 @@ typedef struct Node{
 
 // Criar o Node:
 Node* createNode(int value){
-    Node* node =(Node*)malloc(sizeof(Node));
-    if(node == NULL){
+    Node* node = (Node*)malloc(sizeof(Node));
+    if (node == NULL) {
         printf("Falha ao alocar a memória.");
     }
+    
     // Parâmetros 
-    node->value = value;
-    node->altura = 1;
-    node->direita = NULL;
-    node->esquerda = NULL;
+        node->value = value;
+        node->altura = 1;
+        node->direita = NULL;
+        node->esquerda = NULL;
+    
     return node;
 }
 
 int pegarAltura(Node* node){
-    if(node == NULL){
+    if  (node == NULL)  {
         return 0;
     }
+    
     return node->altura;
 }
 
 int fatorBalanceamento(Node* node){
-    if (node == NULL){
+    if (node == NULL)   {
         return 0;
     }
-    int alturaEsq = pegarAltura(node->esquerda);
-    int alturaDir = pegarAltura(node->direita);
+        int alturaEsq = pegarAltura(node->esquerda);
+        int alturaDir = pegarAltura(node->direita);
+    
     return alturaDir - alturaEsq;
 }
 
@@ -59,20 +63,30 @@ void updateAltura(Node* node){
     }
 }
 
-/* NULL @@@@@@@@ will be @@@@@@@ Node:   [value]
+/* NULL @@@@@@@@ will be @@@@@@@ Node: 
+                                         [value]
                                         /       \                   
                                        esq      dir
                                         
-                                       
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                           
 */
+
+/*BST - menores à esquerda, maiores à direita*/
 
 Node* insert(Node* node, int value){
     if (node == NULL){
-        node->value = value;
-        node->altura = 1;
+        return createNode(value);
     }
+    if (value < node->value) {
+        node->esquerda = insert(node->esquerda, value);
+    } else if (value > node->value) {
+        node->direita = insert(node->direita, value);
+    } else {
+    // value == node->value
+        return node;
+    }
+    return node;
 }
-
 
 
 int main(){
